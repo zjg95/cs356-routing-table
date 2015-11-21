9fe5968ddddf387fabc33913eacf1ee8bb0e0ca0
Zachary Goodman <zjgoodman@utexas.edu>
2015-11-21 13:19:21 -0600

    added basic network methods

--- server-zjg95.py
+++ server-zjg95.py
@@ -34,6 +34,20 @@ def getPort () :
 		exit()
 	return int(argv[1])
 
+# -----------
+# get request
+# -----------
+
+def getRequest (socket) :
+	"""
+    receive bytes from a socket
+    socket the socket of the client
+    return a string containing the client's request
+    """
+	request = socket.recv(MAX_FILE_SIZE)
+	request = bytes.decode(request)
+	return request
+
 # ---------------
 # return response
 # ---------------
@@ -44,11 +58,32 @@ def returnResponse (response, socket) :
     response the bytes to send
     socket the client socket to use
     """
-	# return the result to the client
 	socket.send(response)
 
+# ------
+# listen
+# ------
+
 def listen() :
-	pass
+	"""
+    listen for clients, accept the connection, handle the request, close the connection
+    """
+	while True:
+
+		# accept the connection to a client
+		clientSocket, addr = serverSocket.accept()
+
+		# obtain the request through the wire
+		rawRequest = getRequest(clientSocket)
+
+		# add the headers to the response
+		rawResponse = getResponse(responseDict)
+
+		# send the response over the wire
+		returnResponse(rawResponse, clientSocket)
+
+		#close the connection
+		clientSocket.close()
 
 # ----
 # main
